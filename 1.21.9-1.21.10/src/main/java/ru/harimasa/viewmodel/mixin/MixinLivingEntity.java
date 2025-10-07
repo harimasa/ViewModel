2d7e9d0e2559b2811db1e81e4ba038fdd181729a
harimasa <144851980+harimasa@users.noreply.github.com>
2025-10-08 00:09:05 +0300

    port to 1.21.9-1.21.10

---
+++ 1.21.9-1.21.10/src/main/java/ru/harimasa/viewmodel/mixin/MixinLivingEntity.java
@@ -0,0 +1,18 @@
+package ru.harimasa.viewmodel.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import ru.harimasa.viewmodel.config.VMScreen;
+
+@Mixin(LivingEntity.class)
+public class MixinLivingEntity {
+    @Inject(method = {"getHandSwingDuration"}, at = {@At("HEAD")}, cancellable = true)
+    private void getArmSwingAnimationEnd(final CallbackInfoReturnable<Integer> info) {
+        if (VMScreen.CONFIG.instance().animEnable && VMScreen.CONFIG.instance().slowAnim) {
+            info.setReturnValue(VMScreen.CONFIG.instance().slowAnimValue);
+        }
+    }
+}
